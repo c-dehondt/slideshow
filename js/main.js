32c50f6e124fb9db6ccff17cbfa66bd3657f32c1
c.dehondt <c.dehondt@live.com>
2017-07-17 11:16:09 +0200

    project slidshow finish step 1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 var image = 0;
 var slideImage = 1;
 var slide = document.getElementsByClassName("monSlide");
-moin(slideImage);
 carousel();
 
 function carousel() {
@@ -19,11 +18,10 @@ function plus(n) {
 }
 
 function moin(n) {
-    var x = document.getElementsByClassName("monSlide");
-    if (n > x.length) {slideImage = 1}
-    if (n < 1) {slideImage = x.length} ;
-    for (var i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
+    if (n > slide.length) {slideImage = 1}
+    if (n < 1) {slideImage = slide.length} ;
+    for (var i = 0; i < slide.length; i++) {
+        slide[i].style.display = "none";
     }
-    x[slideImage-1].style.display = "block";
+    slide[slideImage-1].style.display = "block";
 }
