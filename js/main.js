ff0ecfa0810be712c6416456849182fe55ac4681
c.dehondt <c.dehondt@live.com>
2017-07-17 10:18:31 +0200

    add the botton for the project slideshow

--- js/main.js
+++ js/main.js
@@ -1,14 +1,29 @@
 var image = 0;
+var slideImage = 1;
+var slide = document.getElementsByClassName("monSlide");
+moin(slideImage);
 carousel();
 
 function carousel() {
-    var i;
-    var x = document.getElementsByClassName("monSlide");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";
+    for (var i = 0; i < slide.length; i++) {
+       slide[i].style.display = "none";
     }
     image++;
-    if (image > x.length) {image = 1}
-    x[image-1].style.display = "block";
+    if (image > slide.length) {image = 1}
+    slide[image-1].style.display = "block";
     setTimeout(carousel, 2000);
+}
+
+function plus(n) {
+    moin(slideImage += n);
+}
+
+function moin(n) {
+    var x = document.getElementsByClassName("monSlide");
+    if (n > x.length) {slideImage = 1}
+    if (n < 1) {slideImage = x.length} ;
+    for (var i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideImage-1].style.display = "block";
 }
